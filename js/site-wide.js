56ae48a947a2288ae0dedc9fc0f435f548ed51c1
Jez <jthompson@lincoln.ac.uk>
2018-03-20 10:39:55 +0000

    JS tweak

--- js/site-wide.js
+++ js/site-wide.js
@@ -36,56 +36,13 @@ jQuery(document).ready(function($) {
 	mobileMenuLaunch();
 
 
-//	Reading Time Plugin
-
-	var $article = $('.blog-article');
-
-		$article.readingTime({
-		    readingTimeAsNumber: false,
-			readingTimeTarget: ('.eta'),
-			wordCountTarget: $article.find('.word-count'),
-			wordsPerMinute: 275,
-			lessThanAMinuteString: ('less than a minute'),
-			round: true,
-			lang: 'en',
-			success: function() {
-//				console.log('It worked!');
-			},
-			error: function(message) {
-//				console.log(message);
-				$article.find('.reading-time').remove();
-			}
-		});
-
-
-
-//	Slider Plugin
-
-	$('.hero-header').slick({
-		dots: true,
-		pauseOnHover: true,
-		arrows: false,
-		slidesToShow: 1,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 2500
-	});
-
-
-
-//	Comment Tabs
-
-$('ul.comment-tabs li').click(function(){
-
-		var tab_id = $(this).attr('data-tab');
-
-		$('ul.comment-tabs li').removeClass('current');
-		$('.comment-tab__tab-content').removeClass('current');
-
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
-
+// Toggle search form
+$(".search-toggle").on("click", function(){	
+	$(this).toggleClass("active");
+	$(".header-search-block").slideToggle();
+	$(".header-search-block #s").focus();
+	return false;
+});
 
 
 });
