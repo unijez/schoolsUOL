8dd169b2041998d22f1bfdd96c07b14f3b3ade7b
Jez <jthompson@lincoln.ac.uk>
2018-06-04 14:49:16 +0100

    Plugin updater

--- functions.php
+++ functions.php
@@ -193,3 +193,19 @@ require get_template_directory() . '/inc/functions-acf.php';
 if ( file_exists( get_template_directory() . '/inc/customizer/wpc-init.php' ) ) {
     require_once( get_template_directory() . '/inc/customizer/wpc-init.php' );
 }
+
+
+
+/**
+ * Load Theme Update file.
+ */
+if ( file_exists( get_template_directory() . '/inc/plugin-update-checker/plugin-update-checker.php' ) ) { 
+
+	require get_template_directory() .  '/inc/plugin-update-checker/plugin-update-checker.php';
+	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+		'https://wordpress.uat.lincoln.ac.uk/themes/schoolsBlog/schoolsBlog-theme-update.json',
+		__FILE__, //Full path to the main plugin file or functions.php.
+		'schoolsBlog'
+	);
+	
+}
