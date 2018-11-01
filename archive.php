0ef00b1048602d998be4d302d68641f06fdb737d
Brennan Ceballos Pena <bpena@lincoln.ac.uk>
2018-11-01 16:00:04 +0000

    Amended: Removed the cat, auth and tag

--- archive.php
+++ archive.php
@@ -18,101 +18,101 @@
 
 
 	<div class="module-inner-wrap">
-		 			
-		 			
+
+
 		 			<div class="post-head post-archive">
-		 				
+
 		 					<h4 class="post-title">
-		 						
-		 							<?php 
+
+		 							<?php
 		 							if ( is_day() ) :
 		 								printf( __( 'Date: %s', 'schoolsUOL' ), get_the_date() );
-		 			
+
 		 							elseif ( is_month() ) :
 		 								printf( __( 'Month: %s', 'schoolsUOL' ), get_the_date( _x( 'F Y', 'F = Month, Y = Year', 'schoolsUOL' ) ) );
-		 			
+
 		 							elseif ( is_year() ) :
 		 								printf( __( 'Year: %s', 'schoolsUOL' ), get_the_date( _x( 'Y', 'Y = Year', 'schoolsUOL' ) ) );
-		 			
+
 		 							elseif ( is_category() ) :
-		 								printf( __( 'Category: %s', 'schoolsUOL' ), single_cat_title( '', false ) . '' );
-		 			
+		 								printf( __( '%s', 'schoolsUOL' ), single_cat_title( '', false ) . '' );
+
 		 							elseif ( is_tag() ) :
-		 								printf( __( 'Tag: %s', 'schoolsUOL' ), single_tag_title( '', false ) . '' );
-		 			
+		 								printf( __( '%s', 'schoolsUOL' ), single_tag_title( '', false ) . '' );
+
 		 							elseif ( is_author() ) :
 		 								$curauth = ( isset( $_GET['author_name'] ) ) ? get_user_by( 'slug', $author_name ) : get_userdata( intval( $author ) );
-		 								printf( __( 'Author: %s', 'schoolsUOL' ), $curauth->display_name );
-		 								
-		 							else : 
-		 								_e( 'Archive', 'schoolsUOL' ); 
+		 								printf( __( '%s', 'schoolsUOL' ), $curauth->display_name );
+
+		 							else :
+		 								_e( 'Archive', 'schoolsUOL' );
 		 							endif;
-		 			
-		 							
+
+
 		 							?>
-		 			
+
 		 					</h4>
-		 						
+
 		 						<?php $paged = get_query_var( 'paged' ) ?: 1;
-		 							
+
 		 							if ( 1 < $wp_query->max_num_pages ) : ?>
-		 							
+
 		 								<h3><?php printf( __( '(page %s of %s)', 'schoolsUOL' ), $paged, $wp_query->max_num_pages ); ?></h3>
-		 							
+
 		 							<?php endif; ?>
-		 						
-		 						
+
+
 		 						<?php
 		 						$tag_description = tag_description();
-		 						
+
 		 						if ( ! empty( $tag_description ) ) {
 		 							echo apply_filters( 'tag_archive_meta', '<div class="tag-meta">' . $tag_description . '</div>' );
 		 						}
 		 						?>
-		 			
+
 		 				</div>  <!--post-head-->
-		 	
-		
+
+
 
 
 		 				<?php if ( have_posts() ) : ?>
-		 				
+
 		 				<div class="posts-wrapper columns-wrap">
-		 				
-		 				
+
+
 		 				<?php while ( have_posts() ) : the_post(); ?>
-		 					
+
 		 					<div class="post-article-container column-spacings column column--1-of-3  column--small-1-of-2">
-		 					
-		 				
+
+
 		 						<article id="post-article-<?php the_ID(); ?>" <?php post_class(); ?>>
-		 						
+
 									<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		 							
+
 		 						</article>
-		 					
-		 					
+
+
 		 					</div> <!--post-article-container-->
-		 					
+
 		 				<?php endwhile; ?>
-		 				
+
 		 				</div> <!--posts-wrapper-->
-		
-		 			
+
+
 					<?php endif;  wp_reset_query(); ?>
-						
-						
-		 	
-		 	
+
+
+
+
 		 				</div> <!--posts-wrapper-->
-			
-	
-			
+
+
+
 	</div> <!--module-inner-wrap-->
-	
+
 		<?php get_template_part( 'template-parts/site/loop', 'nav' ); ?>
-	
-	
+
+
 </section>
 
 
